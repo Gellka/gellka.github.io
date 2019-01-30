d6619b51cabd1026fc5a0aba42f6d639e8aa528c
Gellka <32881461+Gellka@users.noreply.github.com>
2019-01-30 17:37:57 +0300

    Add files via upload

---
+++ js/antext.js
@@ -0,0 +1,23 @@
+﻿$.fn.animate_Text = function() {
+  var string = this.text();
+  return this.each(function(){
+   var $this = $(this);
+   $this.html(string.replace(/./g, '<span class="new">$&</span>'));
+   $this.find('span.new').each(function(i, el){
+    setTimeout(function(){ $(el).addClass('div_opacity'); }, 20 * i);
+   });
+  });
+ };
+
+$(function() {
+    var otop = $('.example').offset().top - window.innerHeight;
+    $(window).scroll(function(){
+        if($('.example').is(":hidden")){
+            var ptop = $('body').scrollTop();
+            if(ptop > otop){
+                $('.example').show();
+                $('.example').animate_Text();
+            }
+        }
+    });
+});
